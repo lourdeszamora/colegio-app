adf1d30581af8e22023c7fb8f40a45ca6ceefe97
Lourdes Zamora <lourdes.graciela.zamora@gmail.com>
2024-05-16 15:39:39 -0600

    structure

---
+++ src/app/alumnos/page.tsx
@@ -0,0 +1,102 @@
+'use client';
+import { Card, Col, Row, Space, Table, TableProps, Tag } from 'antd';
+import React, { use, useState } from 'react';
+
+interface Props {
+  name: string;
+}
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size='middle'>
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const Alumnos: React.FC<Props> = ({ name }) => {
+  return (
+    <Row justify={'center'} >
+      <Col xs={24} sm={24} md={24} lg={16} xl={16} xxl={16}>
+        <Card title='Alumnos'>
+          <Table columns={columns} dataSource={data} />
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default Alumnos;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,69 @@
-"use client";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+'use client';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 import {
-  DesktopOutlined,
   FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
 } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { useState } from "react";
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout, Menu } from 'antd';
+import { useState } from 'react';
+const { Content, Sider } = Layout;
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UnorderedListOutlined , IdcardOutlined} from '@ant-design/icons';
 
-const inter = Inter({ subsets: ["latin"] });
-
-
+const inter = Inter({ subsets: ['latin'] });
+const queryClient = new QueryClient();
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const [collapsed, setCollapsed] = useState(false);
+
+  const items: MenuItem[] = [
+    {
+      key: 'profesores-link',
+      label: <a href='/profesores'>Profesores</a>,
+      icon: <UnorderedListOutlined />
+    },
+    {
+      key: 'grados-link',
+      label: <a href='/grados'>Grados</a>,
+      icon: <FileOutlined />,
+    },
+    {
+      key: 'alumnos-link',
+      label: <a href='/alumnos'>Alumnos</a>,
+      icon: <IdcardOutlined />,
+    },
+  ];
+
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Layout>
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-          <div className="demo-logo-vertical" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline"  />
+    <html lang='en'>
+      <body>
+      <QueryClientProvider client={queryClient}>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className='demo-logo-vertical' />
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline' items={items} />
         </Sider>
-        <Layout>
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-              }}
-            >
-              {children}
-            </div>
-          </Content>
-        </Layout>
+        <Content style={{ margin: '0 16px', height: '100%' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            {children}
+          </div>
+        </Content>
       </Layout>
+      </QueryClientProvider>
       </body>
-  </html>
+    </html>
   );
 }
