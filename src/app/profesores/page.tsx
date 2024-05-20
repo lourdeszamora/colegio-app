99ae66145f2b2713361b788bebd1f274562f2b2a
Lourdes Zamora <lourdes.graciela.zamora@gmail.com>
2024-05-20 03:26:42 -0600

    post profesor

--- src/app/profesores/page.tsx
+++ src/app/profesores/page.tsx
@@ -1,16 +1,27 @@
 'use client';
-import { useQuery } from '@tanstack/react-query';
-import { Button, Card, Col, Row, Space, Table, TableProps, Tag } from 'antd';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  Button,
+  Card,
+  Col,
+  message,
+  Row,
+  Space,
+  Table,
+  TableProps,
+  Tag,
+} from 'antd';
 import React, { useMemo } from 'react';
 import profesoresService from '../../services/profesores.service';
 import FormModal from '@/components/general/form-modal';
 import ProfesorForm from '@/components/profesor/ProfesorForm';
-
-
+import { on } from 'events';
+import Profesor from '@/models/profesor.entity';
 
 const Profesores = () => {
   const [page, setPage] = React.useState(1);
   const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const queryClient = useQueryClient();
   const {
     isLoading,
     isError,
@@ -20,6 +31,20 @@ const Profesores = () => {
     queryKey: ['profesores', page],
     queryFn: ({ signal }) => profesoresService.getAll(page, 20, signal),
   });
+  const createProfesor = useMutation({
+    mutationFn: (data: Profesor) => profesoresService.create(data),
+    onSuccess: (data: Profesor) => {
+      queryClient.invalidateQueries({ queryKey: ['profesores', page] });
+      message.success(
+        `El Profesor ${data.nombre} ${data.apellidos} fue agregado!`,
+      );
+      setIsModalVisible(false);
+    },
+    onError: (error) => {
+      message.error('No se pudo guardar el profesor, intente de nuevo!');
+    },
+  });
+
   const columns = useMemo(
     () => [
       {
@@ -59,6 +84,8 @@ const Profesores = () => {
         title='Nuevo profesor'
         onClose={() => setIsModalVisible(false)}
         okText='Guardar'
+        isLoading={createProfesor.isPending}
+        onSubmit={createProfesor.mutate}
       >
         <ProfesorForm />
       </FormModal>
@@ -66,7 +93,9 @@ const Profesores = () => {
         <Card title='Profesores'>
           <Row justify='end'>
             <Col>
-              <Button type='primary' onClick={() => setIsModalVisible(true)}>Nuevo</Button>
+              <Button type='primary' onClick={() => setIsModalVisible(true)}>
+                Nuevo
+              </Button>
             </Col>
           </Row>
           <Table
--- src/components/profesor/ProfesorForm.tsx
+++ src/components/profesor/ProfesorForm.tsx
@@ -1,47 +1,51 @@
 import React from 'react';
-import { Form, Input, Button, FormInstance } from 'antd';
-import  Profesor from '../../models/profesor.entity';
+import { Form, Input, Button, FormInstance, Select } from 'antd';
+import Profesor from '../../models/profesor.entity';
 import { IFormProps } from '@/models/form-props.interface';
+import Genero from '@/models/genero.entity';
 
+const ProfesorForm = ({ form, onSubmit }: IFormProps<Profesor>) => {
+  const onFinish = (values: Profesor) => {
+    console.log('Form values:', values);
+  };
 
+  return (
+    <Form form={form} onFinish={onSubmit}>
+      <Form.Item
+        label='Id'
+        name='id'
+        rules={[{ required: true, message: 'Debe ingresar los nombres' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label='Nombres'
+        name='nombre'
+        rules={[{ required: true, message: 'Debe ingresar los nombres' }]}
+      >
+        <Input />
+      </Form.Item>
 
-const ProfesorForm = ({form, onSubmit}: IFormProps<Profesor>) => {
-    const onFinish = (values: Profesor) => {
-        console.log('Form values:', values);
-    };
+      <Form.Item
+        label='Apellidos'
+        name='apellidos'
+        rules={[{ required: true, message: 'Debe ingresar los apellidos' }]}
+      >
+        <Input />
+      </Form.Item>
 
-    return (
-        <Form form={form} onFinish={onSubmit} >
-            <Form.Item hidden name="id">
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label="Nombres"
-                name="nombres"
-                rules={[{ required: true, message: 'Debe ingresar los nombres' }]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Apellidos"
-                name="apellidos"
-                rules={[{ required: true, message: 'Debe ingresar los apellidos' }]}
-            >
-                <Input/>
-            </Form.Item>
-
-            <Form.Item
-                label="Genero"
-                name="genero"
-                rules={[
-                    { required: true, message: 'Especifique el genero' },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-        </Form>
-    );
+      <Form.Item
+        label='Genero'
+        name='genero'
+        rules={[{ required: true, message: 'Especifique el genero' }]}
+      >
+        <Select placeholder='Seleccione el genero'>
+            <Select.Option value={Genero.Masculino}>Masculino</Select.Option>
+            <Select.Option value={Genero.Femenino}>Femenino</Select.Option>
+        </Select>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default ProfesorForm;
